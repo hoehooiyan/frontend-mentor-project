82d39fb1251e446a76eb08f1136a9e6ef3e26430
hoehooiyan <hoehooiyan@gmail.com>
2020-02-09 12:43:45 +0800

    600px cta section respopnsive done

--- url-shortening-api-landing-page/src/components/cta/cta.component.jsx
+++ url-shortening-api-landing-page/src/components/cta/cta.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useMediaQuery } from '../../hooks/useMediaQuery';
 
 import globalStyles from '../../app/App.styles.scss';
 import ctaStyles from './cta.module.scss';
@@ -6,6 +7,8 @@ import ctaStyles from './cta.module.scss';
 import Button from '../button/button.component';
 
 const Cta = () => {
+  const lMobileButton = useMediaQuery(`(max-width: ${globalStyles.lMobile})`);
+
   return (
     <section className={ctaStyles.cta}>
       <h2 className={ctaStyles.title}>Boost your links today</h2>
@@ -13,12 +16,13 @@ const Cta = () => {
         backgroundColor={globalStyles.primaryCyan}
         borderRadius='50rem'
         color='#ffffff'
+        fontSize={lMobileButton ? '1.8rem' : null}
         padding='1.2rem 2rem'
         text='Get Started'
-        width='15rem'
+        width={lMobileButton ? '18rem' : '15rem'}
       />
     </section>
-  )
-}
+  );
+};
 
 export default Cta;
